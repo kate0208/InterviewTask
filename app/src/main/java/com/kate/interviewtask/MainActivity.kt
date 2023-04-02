b3df819f15180fc83c70b18429d93f5148e69e36
Kate <a0921188994@gmail.com>
2023-04-02 23:16:30 +0800

    add toolBar

--- app/src/main/java/com/kate/interviewtask/MainActivity.kt
+++ app/src/main/java/com/kate/interviewtask/MainActivity.kt
@@ -2,6 +2,9 @@ package com.kate.interviewtask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.kate.interviewtask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = getString(R.string.main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
 }
--- app/src/main/java/com/kate/interviewtask/fragment/ImageViewerFragment.kt
+++ app/src/main/java/com/kate/interviewtask/fragment/ImageViewerFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.kate.interviewtask.databinding.FragmentImageViewerBinding
 import com.kate.interviewtask.viewmodel.ImageViewerViewModel
+
 
 class ImageViewerFragment : Fragment() {
     private var _binding: FragmentImageViewerBinding? = null
@@ -30,6 +32,7 @@ class ImageViewerFragment : Fragment() {
 
         viewModel.source.observe(viewLifecycleOwner, Observer {
             if (it != null) {
+                (activity as AppCompatActivity).supportActionBar?.title = it.title
                 Glide.with(this)
                     .load(it.hdurl)
                     .into(binding.image)
