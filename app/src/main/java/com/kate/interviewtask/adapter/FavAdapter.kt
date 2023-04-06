2a9d295511f0bed17f7899c7f7db349bdeb4d34d
Kate <a0921188994@gmail.com>
2023-04-06 14:30:36 +0800

    add string resource

--- app/src/main/java/com/kate/interviewtask/adapter/FavAdapter.kt
+++ app/src/main/java/com/kate/interviewtask/adapter/FavAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.kate.interviewtask.R
 import com.kate.interviewtask.databinding.SourceItemBinding
 import com.kate.interviewtask.model.SourceModel
 
@@ -48,9 +49,9 @@ class FavItemViewHolder(
         }
 
         if (item.fav) {
-            binding.button.text = "移除最愛"
+            binding.button.text = itemView.context.getString(R.string.fav_remove)
         } else {
-            binding.button.text = "加入最愛"
+            binding.button.text = itemView.context.getString(R.string.fav_add)
         }
     }
 
